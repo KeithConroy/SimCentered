5f2faa49c141539d972a09af5e7fc55f3ad745f7
keithconroy <kconroyjr@gmail.com>
2015-08-06 18:37:56 -0700

    add basic crud for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :find_organization
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -9,6 +10,13 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.organization_id = @organization.id
+    if @event.save
+      redirect_to organization_event_path(@organization.id, @event.id)
+    else
+      render json: "no"
+    end
   end
 
   def show
@@ -18,14 +26,29 @@ class EventsController < ApplicationController
   end
 
   def update
+    if @event.update_attributes(event_params)
+      redirect_to organization_event_path(@organization.id, @event.id)
+    else
+
+    end
   end
 
   def destroy
+    @event.destroy
+    redirect_to(:action => 'index')
   end
 
   private
 
   def find_organization
     @organization = Organization.where(id: params[:organization_id]).first
+  end
+
+  def find_event
+    @event = Event.where(id: params[:id]).first
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :date, :time, :organization_id)
   end
 end
