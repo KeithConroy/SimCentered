c5cf2f698217d835ddb68351812f383d21ecd82a
keithconroy <kconroyjr@gmail.com>
2015-08-25 20:28:55 -0700

    full seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Organization.create(title: "SimCentered University")
+Organization.create(title: "SimCentered Nursing")
+User.create(first_name: "Keith", last_name: "Conroy", email: "keith@mail.com", password: "123", organization_id: 1)
 
 10.times do
   User.create(
@@ -15,14 +18,32 @@
   )
 end
 
-# 10.times do |num|
-#   Room.create(
-#     title: "Exam Room #{num+1}",
-#     number: Faker::Number.number(3),
-#     building: Faker::Company.name,
-#     organization_id: 1
-#   )
-# end
+10.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    organization_id: 2
+  )
+end
+
+10.times do |num|
+  Room.create(
+    title: "Exam Room #{num+1}",
+    number: Faker::Number.number(3),
+    building: Faker::Company.name,
+    organization_id: 1
+  )
+end
+
+10.times do |num|
+  Room.create(
+    title: "Exam Room #{num+1}",
+    number: Faker::Number.number(3),
+    building: Faker::Company.name,
+    organization_id: 2
+  )
+end
 
 # Organization.create(
 #   title: Faker::University.name
