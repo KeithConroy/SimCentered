0479ad47701b73330999a9187037ec96dee9f56d
keithconroy <kconroyjr@gmail.com>
2015-08-26 15:51:42 -0700

    reorder date inputs on forms, fixed todays events query

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -13,7 +13,7 @@ class OrganizationsController < ApplicationController
   def show
     @organization = Organization.where(id: params[:id]).first
     @event = Event.new()
-    @events = Event.where(organization_id: @organization.id, start: Date.today).order(start: :asc)
+    @events = Event.where(organization_id: @organization.id).where('start BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).order(start: :asc)
   end
 
   def edit
