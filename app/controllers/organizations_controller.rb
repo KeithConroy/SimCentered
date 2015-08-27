e18a7a0635be1ac239eb31b177e10a922ef107a4
keithconroy <kconroyjr@gmail.com>
2015-08-26 17:11:34 -0700

    add subdomain field, edit and delete links

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,5 @@
 class OrganizationsController < ApplicationController
+  before_action :find_organization
 
   def index
 
@@ -12,10 +13,11 @@ class OrganizationsController < ApplicationController
     @organization = Organization.new(organization_params)
     if @organization.save
       @admin = User.create!(
-        first_name: organization_params[:title],
+        first_name: organization_params[:subdomain],
         last_name: "Admin",
         email: organization_params[:email],
         password: "",
+        is_student: false,
         organization_id: @organization.id
       )
       redirect_to organization_path(@organization.id)
@@ -25,7 +27,6 @@ class OrganizationsController < ApplicationController
   end
 
   def show
-    @organization = Organization.where(id: params[:id]).first
     @event = Event.new()
     @events = Event.where(organization_id: @organization.id).where('start BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).order(start: :asc)
   end
@@ -34,6 +35,11 @@ class OrganizationsController < ApplicationController
   end
 
   def update
+    if @organization.update_attributes(organization_params)
+      redirect_to @organization
+    else
+
+    end
   end
 
   def destroy
@@ -41,7 +47,11 @@ class OrganizationsController < ApplicationController
 
   private
 
+  def find_organization
+    @organization = Organization.where(id: params[:id]).first
+  end
+
   def organization_params
-    params.require(:organization).permit(:title, :email)
+    params.require(:organization).permit(:title, :subdomain, :email)
   end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,8 @@
 class Organization < ActiveRecord::Base
+
   attr_accessor :email
-  validates_presence_of :title
-  validates_uniqueness_of :title
+
+  validates_presence_of :title, :subdomain
+  validates_uniqueness_of :title, :subdomain
+
 end
