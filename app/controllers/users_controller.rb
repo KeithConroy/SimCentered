48fc1555197eef5c2217b2f4a5508e9e411fc8b7
keithconroy <kconroyjr@gmail.com>
2015-08-26 21:21:10 -0700

    add user#new page, add sort icons to index pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :is_student, :password)
   end
 end
