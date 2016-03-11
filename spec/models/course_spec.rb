305de6f50d2a495266ec1d1d43db5b4529ac76fc
keithconroy <kconroyjr@gmail.com>
2016-03-10 22:18:31 -0800

    add validation failure tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,8 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  it "fails validation with no title (using error_on)" do
-    # expect(Course.new).to have(1).error_on(:title)
-    # expect(Course).to validate_presence_of(:title)
+  let(:organization) do
+    Organization.create!(
+      title: "University",
+      subdomain: "uni"
+    )
+  end
+  let(:instructor) do
+    User.create!(
+      first_name: "Keith",
+      last_name: "Conroy",
+      email: "keith@mail.com",
+      organization_id: organization.id,
+      is_student: false,
+    )
+  end
+  let(:course) do
+    Course.create!(
+      title: "Test Course",
+      instructor_id: instructor.id,
+      organization_id: organization.id,
+    )
+  end
+  context 'validation' do
+    it "fails validation with no title" do
+      course = Course.new(
+        instructor_id: instructor.id,
+        organization_id: organization.id,
+      )
+      course.valid?
+      expect(course.errors[:title].size).to eq(1)
+    end
+    it "fails validation with no organization_id" do
+      course = Course.new(
+        title: "Test Course",
+        instructor_id: instructor.id,
+      )
+      course.valid?
+      expect(course.errors[:organization_id].size).to eq(1)
+    end
   end
 end
