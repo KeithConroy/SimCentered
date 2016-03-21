a1fc0d9ebdf504d988a7632bf80d9281a7ccfa0f
keithconroy <kconroyjr@gmail.com>
2016-03-20 19:53:09 -0700

    clean up, add authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :find_organization, :set_time_zone
+  before_filter :find_organization, :set_time_zone, :authenticate_user!
 
   def find_organization
     @organization = Organization.where(id: current_user.organization_id).first if current_user
-    if params[:organization_id] && params[:organization_id].to_i != @organization.id
-      render file: "public/401.html", status: :unauthorized
+    if params[:organization_id] && @organization
+      if params[:organization_id].to_i != @organization.id
+        render file: "public/401.html", status: :unauthorized
+      end
     end
   end
 
