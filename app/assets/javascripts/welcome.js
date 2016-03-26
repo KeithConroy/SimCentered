b1c13494cd4b0be5244fff528061e4a13969894f
keithconroy <kconroyjr@gmail.com>
2016-03-26 01:20:54 -0700

    create changing background effect

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,15 @@
 $(document).on('welcome:loaded', function() {
-  $('body').scrollspy({ target: '.navbar-custom' })
+  $('body').scrollspy({ target: '.navbar-custom' });
+
+  $(window).scroll(function(){
+    var fromTopPx = $(window).height(); // distance to trigger
+    var scrolledFromtop = $(window).scrollTop();
+    if(scrolledFromtop > fromTopPx){
+      $('#welcome').addClass('scrolled');
+      $('#welcome').removeClass('doctor');
+    }else{
+      $('#welcome').addClass('doctor');
+      $('#welcome').removeClass('scrolled');
+    }
+  });
 });
