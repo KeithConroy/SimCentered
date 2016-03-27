a5a7e0adf6705f3602b8877028b0a9caf18e9cf8
keithconroy <kconroyjr@gmail.com>
2016-03-27 16:07:32 -0700

    add text animation, new background

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,4 +15,23 @@ $(document).on('home:loaded', function() {
       $('#home').addClass('doctor');
     }
   });
+
+  $('.centered').hide();
+  setTimeout(function() {
+    $('.simplify').fadeIn(1000);
+  },500);
+
+  setTimeout(function() {
+    $('.simplify').fadeOut(1000);
+    setTimeout(function(){
+      $('.simulation').fadeIn(1000);
+    },1000);
+    setTimeout(function(){
+      $('.simulation').fadeOut(1000);
+    },2500);
+    setTimeout(function(){
+      $('.centered').fadeIn(1000);
+    },3500);
+  },2000);
+
 });
